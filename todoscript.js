51e18f480f1bb6ac3a05d9039888721900d14966
Gracii <griseldacastaneda2013@gmail.com>
2020-10-20 20:33:36 -0700

    Eight commit

--- todoscript.js
+++ todoscript.js
@@ -1,45 +1,48 @@
 
-//Select Elements  and add eventlisteners
-// check if input is working with a console.log
-//create a variable that will store get.elementbyId for the input element.
-let inputValue = document.getElementById('input');
-inputValue.addEventListener('keyup', function(e){
-//retrieve  and store the value from the input box
-let inputBoxValue = e.target.value;
+//Select Elements, eventlisteners and functions: input new data, add a new task, add add the data, delete the list.  
 
+//1. create a variable that will store get.elementbyId for the input element.
+//check if input is working with a console.log
+const input = document.getElementById('input');
+//input.addEventListener('click', function(e){
+//retrieve  and store the value from the input box
+//let inputBoxValue = e.target.value;
 //console.log(inputBoxValue);
 
-let clearInput = document.getElementById("clearAll");clearInput.addEventListener("click", clearInput);
- console.log(clearInput);
-//I need the input data to be deleted when I click on the button ClearAll 
+//});
 
-});
+// add a const 
+//2. get element by id for button that will add items to list , we can break it up to make it easier 
+const submitBtn = document.getElementById('add-button');
+submitBtn.addEventListener('click', addTodo);
+  
+function addTodo (event) {
+    event.preventDefault();   
+    console.log('clicked');
+    const listItem = input.value;
+    console.log(listItem);
+    
 
-document.getElementById("addButton").addEventListener("click", inputValue);
-//console.log(inputValue);
-
-
-
-//clear to .addeventlistener to click to clear the list 
-
-// functions
-//add add the data
-//function add task
-// clear the list 
-//delete the list 
-
-// get element by id for button that will add items to list
-// get element by id for ul list
-
-// create a new li element
-// document.createElement('li);
-
+//get element by id for ul list
+//create a new li element // document.createElement('li);
+//display that list item to the page
 // add class to li 
 // li.className ????
-
 // add text node with input value appendChild
 // appendChild to ul parent 
-// 
+
+}
+//3. I need the input data to be cleared when I click on the button ClearAll 
+const ClearAll = document.getElementById('clear-all');
+ClearAll.addEventListener('click', clearTodo); 
+
+function clearTodo (event) {
+    console.log('clear all clicked');
+    input.value ="";
+}
+
+
+ 
 
 
 
