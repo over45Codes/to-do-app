186d39c5da75d0109f07fb9d971754ee094a0897
Grace Castaneda <admin@Mac-mini-de-mac.local>
2020-10-27 21:32:03 -0700

    updates commit

--- todoscript.js
+++ todoscript.js
@@ -1,46 +1,39 @@
 
-const input = document.getElementById('input');
+
 
 const submitBtn = document.getElementById('add-button');
-submitBtn.addEventListener('click', addTodo);
+submitBtn.addEventListener('click', renderTodo);
 
-function addTodo(event) {
+function renderTodo(event) {
     event.preventDefault();
-    console.log('clicked');
-    renderTodo();
-}
-function renderTodo() {
-    let listItem = document.getElementById('list-items');
-    console.log(listItem);
-    let ul = document.querySelector('ul');
-    let li = document.createElement('li');
-    ul.appendChild(li);
-    li.textContent = input.value;
-    if (input.value != '');
-    (input.value);
-    input.value = '';
+    if (!input.value) {
+        alert("Please add a todo");
+    } else {
+        const input = document.getElementById('input');
+        const ul = document.querySelector('ul');
 
-    let deleteBtn = document.createElement('button');
-    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-    deleteBtn.appendChild(document.createTextNode('X'));
-    li.appendChild(deleteBtn);
-    listItem.appendChild(li);
-}
-document.querySelector('ul').addEventListener('click', deleteItem);
+        const li = document.createElement('li');
+        ul.appendChild(li);
+        li.textContent = input.value;
 
-function deleteItem(e) {
-    console.log(e.target.parentElement);
-    let remove = e.target.parentElement;
-    let parentElement = remove.parentElement;
-    parentElement.removeChild(remove);
-}
-const ClearAll = document.getElementById('clear-all');
-ClearAll.addEventListener('click', clearAll);
 
-function clearAll(e) {
-    console.log('clear all clicked');
+        let deleteBtn = document.createElement('button');
+        deleteBtn.className = 'delete';
+        deleteBtn.textContent = 'X';
+
+        li.appendChild(deleteBtn);
+        ul.appendChild(li);
+    }
+    input.value = "";
+}
+const list = document.querySelector('ul');
+list.addEventListener('click', (e) => e.target.parentElement.remove());
+
+const clearAll = document.getElementById('clear-all');
+clearAll.addEventListener('click', clearList);
+
+function clearList(e) {
     const listItems = document.querySelectorAll('li');
-    listItems.forEach((li) => {
-        li.remove();
-    });
+    listItems.forEach((li) => li.remove());
+
 }
