d5f3005a0849a8c57ed156b3336097d5d05c61ae
Grace Castaneda <griseldacastaneda2013@gmail.com>
2020-11-05 21:22:38 -0800

    commit to master 'updating files'

--- todoscript.js
+++ todoscript.js
@@ -1,3 +1,14 @@
+//Console log in local storage//
+//localStorage.setItem("myObject", { name: "Jane Smith", age: 30 });
+//let returnValue = localStorage.getItem("myObject");
+//console.log(returnValue);
+//console.log(typeof returnValue);
+//console.log(returnValue.name);
+
+//const storedTodos = [];
+
+//const renderTodo = JSON.parse(localStorage.getItem("todos"));
+
 const input = document.getElementById("input");
 
 const submitBtn = document.getElementById("add-button");
@@ -5,7 +16,7 @@ submitBtn.addEventListener("click", addTodo);
 
 function addTodo(event) {
   event.preventDefault();
-  console.log("clicked");
+  //console.log("clicked");
   renderTodo();
 }
 function renderTodo() {
@@ -14,17 +25,22 @@ function renderTodo() {
   let ul = document.querySelector("ul");
   let li = document.createElement("li");
   ul.appendChild(li);
+  listItem.appendChild(li);
+  //trying to add local storage///
+  //storedTodos.push(renderTodo);
+  //localStorage.setItem("todos", JSON.stringify(storedTodos));
   li.textContent = input.value;
   if (input.value != "");
   input.value;
   input.value = "";
 
   let deleteBtn = document.createElement("button");
-  deleteBtn.className = "btn btn-danger btn-sm delete";
+  deleteBtn.className = "btn btn-danger btn-sm float-right";
   deleteBtn.appendChild(document.createTextNode("X"));
   li.appendChild(deleteBtn);
   listItem.appendChild(li);
 }
+
 document.querySelector("ul").addEventListener("click", deleteItem);
 
 function deleteItem(e) {
@@ -33,6 +49,7 @@ function deleteItem(e) {
   let parentElement = remove.parentElement;
   parentElement.removeChild(remove);
 }
+
 const ClearAll = document.getElementById("clear-all");
 ClearAll.addEventListener("click", clearAll);
 
