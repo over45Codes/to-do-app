e43214a5e261ac50b9ca4609174686a2f6fc5c92
Grace Castaneda <griseldacastaneda2013@gmail.com>
2020-11-08 14:00:14 -0800

    commit to master ' local storage'

--- todoscript.js
+++ todoscript.js
@@ -1,6 +1,6 @@
 //this is going to represent your local storage array - first get out of local storage using the key is 'todos'
 //const
-const savedTodos = [];
+
 const todos = JSON.parse(localStorage.getItem("todos")) || [];
 const input = document.getElementById("input");
 
@@ -9,15 +9,19 @@ submitBtn.addEventListener("click", addTodo);
 
 function addTodo(event) {
   const text = input.value;
+  const todo = {
+    text,
+    id: 1,
+  };
   event.preventDefault();
   renderTodo(text);
   todos.push(text);
   pushToLocalStorage();
-  //console.log("text value is: ", text);
+  console.log("text value is: ", todo);
 }
+
 function renderTodo(text) {
   let listItem = document.getElementById("listItems");
-  //console.log(listItem);
   let ul = document.querySelector("ul");
   let li = document.createElement("li");
   ul.appendChild(li);
@@ -28,26 +32,19 @@ function renderTodo(text) {
   deleteBtn.className = "btn btn-danger btn-sm float-right";
   deleteBtn.appendChild(document.createTextNode("X"));
   li.appendChild(deleteBtn);
-
-  savedTodos.push(text);
-  localStorage.setItem("text", JSON.stringify(savedTodos));
 }
-
 document.querySelector("ul").addEventListener("click", deleteItem);
 
 function deleteItem(e) {
-  //console.log(e.target.parentElement);
   let remove = e.target.parentElement;
   let parentElement = remove.parentElement;
   parentElement.removeChild(remove);
-  localStorage.setItem("text", JSON.stringify(savedTodos));
 }
 
 const ClearAll = document.getElementById("clear-all");
 ClearAll.addEventListener("click", clearAll);
 
 function clearAll(e) {
-  //console.log("clear all clicked");
   const listItems = document.querySelectorAll("li");
   listItems.forEach((li) => {
     li.remove();
@@ -55,10 +52,8 @@ function clearAll(e) {
     localStorage.removeItem("todos");
   });
 }
-//to create something, you need to create a function - to push to local storage
-function saveToLocalStorage() {
-  localStorage.setItem("todos", JSON.stringify(savedTodos));
-}
+
+// } //to create something, you need to create a function - to push to local storage
 
 function pushToLocalStorage() {
   localStorage.setItem("todos", JSON.stringify(todos));
@@ -84,4 +79,4 @@ function renderOnLoad() {
 
 renderOnLoad();
 
-//create individual to do item, create a unique id for to do item in li - this will help with the strikethrough functionality
+//create individual to do item, create a unique id for each to do item in list - this will help with the strikethrough functionality
